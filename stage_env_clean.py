4b7b654e0e2a098d2e20d12a4faed37becbad2e2
saranyajanakraj <108895480+saranyajanakraj@users.noreply.github.com>
2022-07-16 16:18:12 +0530

    Update stage_env_clean.py

--- stage_env_clean.py
+++ stage_env_clean.py
@@ -29,3 +29,8 @@ if __name__ == "__main__":
         sqL = read_sql()
         print(sqL)
         exec_db(sqL)
+    else (input2=='UAT'):
+        params = config('uat_stage','database.ini')
+        sqL = read_sql()
+        print(sqL)
+        exec_db(sqL)
