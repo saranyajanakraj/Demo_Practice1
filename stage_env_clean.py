48a51d8ae7581152d4e75fdd0825f692ee2fafa2
saranyajanakraj <108895480+saranyajanakraj@users.noreply.github.com>
2022-07-16 15:50:15 +0530

    Update stage_env_clean.py

--- stage_env_clean.py
+++ stage_env_clean.py
@@ -23,7 +23,7 @@ def exec_db(sqL):
 if __name__ == "__main__":
     #input2='preprod_stage'
     input2 = os.environ['ENV']
-     print(input2)
+    print(input2)
     if (input2=='PRE-PROD'):
         params = config('preprod_stage','database.ini')
         sqL = read_sql()
