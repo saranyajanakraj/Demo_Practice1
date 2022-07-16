36e78a2b645c176ac966ee369647b4c13167ca8a
EC2 Default User <ec2-user@ip-172-31-41-23.ap-south-1.compute.internal>
2022-07-16 08:51:28 +0000

    changes

---
+++ stage_env_clean.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import psycopg2
+from configparser import ConfigParser
+from stage_db_clean import config
+
+def read_sql():
+    fd = open('stage_clean.sql', 'r')
+    sqlFile = fd.read()
+    fd.close()
+    sqlCommands = sqlFile.split(';')
+    return sqlCommands
+
+def exec_db(sqL):
+    for command in sqL[:-1]:
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(f'{command}')
+        conn.commit()
+        print("DB Cleanup Done")
+    
+   
+if __name__ == "__main__":
+    input2='preprod_stage'
+    if (input2=='preprod_stage'):
+        params = config(input2,'database.ini')
+        sqL = read_sql()
+        exec_db(sqL)
