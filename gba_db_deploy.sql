54c6a902a41e5d40dab18fe68de6daf5655096d3
EC2 Default User <ec2-user@ip-172-31-41-23.ap-south-1.compute.internal>
2022-07-26 05:56:29 +0000

    sql change1

--- gba_db_deploy.sql
+++ gba_db_deploy.sql
@@ -1,1 +1,1 @@
-drop table gba_db.fleet;create table gba_db.fleet(id int,name varchar(10),address varchar(40));Alter table gba.customer add column address varchar(50);
+drop table gba_db.fleet;create table gba_db.fleet(id int,name varchar(10),address varchar(40));Alter table gba_db.customer add column address varchar(50);
